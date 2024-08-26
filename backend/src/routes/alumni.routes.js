f421d92da4d34a6cd4693f86b511a6fd43feb3da
Raksha-Vats <22cs3046@rgipt.ac.in>
2024-08-26 17:14:19 +0530

    fixed bugs in alumni api

--- backend/src/routes/alumni.routes.js
+++ backend/src/routes/alumni.routes.js
@@ -5,7 +5,7 @@ const router = Router();
 
 router.route("/register").post(alumniController.alumniRegister);
 router.route("/login").post(alumniController.alumniLogin);
-router.route("/", alumniController.getAlumni);
+router.route("/").get(alumniController.getAlumni);
 router.route("/:id").get(alumniController.alumniProfile);
 router.route("/:id").put(alumniController.alumniUpdateProfile);
 router.route("/:id").delete(alumniController.alumniDeleteProfile);
