e66e4d5e64cc9d6e6e5fa5fcb2c8842d86ee197f
Ayush Mittal <ayushmittal1100@gmail.com>
2024-09-16 15:02:20 +0530

    Bugs fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,17 +16,14 @@ import ProgramDetails from "./features/mentorship/ProgramDetails";
 import ForumQuestions from "./features/forums/ForumsQuestions";
 import QuestionDiscussion from "./features/forums/QuestionDiscussion";
 import JobDetails from "./features/jobs/JobDetails";
-<<<<<<< HEAD
+import { Provider } from "react-redux";
+import store from "./store";
 import Chat from "./pages/Chat";
 
 const socket = io.connect('http://localhost:8000');
 
 
 
-=======
-import { Provider } from "react-redux";
-import store from "./store";
->>>>>>> 2922602389ed4a4d4b584e9be9a5c5f16d5d996d
 const App = () => {
 
   const [username, setUsername] = useState('agastya');
@@ -60,39 +57,6 @@ const showNotification = (message) => {
 
 
   return (
-<<<<<<< HEAD
-    <BrowserRouter>
-      <Routes>
-        <Route element={<AppLayout />}>
-          <Route index element={<Navigate replace to="dashboard" />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="alumnidirectory" element={<AlumniDirectory />} />
-          <Route path="/alumnidirectory/:year" element={<BatchDetails 
-                                                        username={username}
-                                                        setUsername={setUsername}
-                                                        room={room}
-                                                        setRoom={setRoom}
-                                                        socket={socket}
-                                                        />} />
-          <Route path="chat/:roomName" element={<Chat socket={socket} username={username} />} />
-          <Route path="events" element={<Events />} />
-          <Route path="mentorship" element={<Mentorship />} />
-          <Route path="mentorship/:id" element={<ProgramDetails />} />
-          <Route path="placement" element={<Placement />} />
-          <Route path="placement/:id" element={<JobDetails />} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="discussionforums" element={<DiscussionForums />} />
-          <Route
-            path="/discussionforums/:forumId"
-            element={<ForumQuestions />}
-          />
-          <Route
-            path="/discussionforums/:forumId/questions/:questionId"
-            element={<QuestionDiscussion />}
-          />
-        </Route>
-        <Route path="login" element={<Login />} />
-=======
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
@@ -100,7 +64,14 @@ const showNotification = (message) => {
             <Route index element={<Navigate replace to="dashboard" />} />
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="alumnidirectory" element={<AlumniDirectory />} />
-            <Route path="/alumnidirectory/:year" element={<BatchDetails />} />
+            <Route path="/alumnidirectory/:year" element={<BatchDetails 
+                                                        username={username}
+                                                        setUsername={setUsername}
+                                                        room={room}
+                                                        setRoom={setRoom}
+                                                        socket={socket}
+                                                        />} />
+            <Route path="chat/:roomName" element={<Chat socket={socket} username={username} />} />
             <Route path="events" element={<Events />} />
             <Route path="mentorship" element={<Mentorship />} />
             <Route path="mentorship/:id" element={<ProgramDetails />} />
@@ -118,7 +89,6 @@ const showNotification = (message) => {
             />
           </Route>
           <Route path="login" element={<Login />} />
->>>>>>> 2922602389ed4a4d4b584e9be9a5c5f16d5d996d
 
           <Route path="*" element={<PageNotFound />} />
         </Routes>
