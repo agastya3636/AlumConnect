68a964e138cb957e74b70acfeb8620aae18bd5c6
Agastya Kumar Yadav <124435030+agastya3636@users.noreply.github.com>
2024-11-12 20:04:11 +0530

    fixed

--- frontend/src/ui/Header.jsx
+++ frontend/src/ui/Header.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { FiLogOut, FiUser } from "react-icons/fi";
-
-
+import { useNavigate } from 'react-router-dom';
+const Navigate = useNavigate();
 function handleLogout () {
   alert("Logging out");
   localStorage.removeItem("token");
   sessionStorage.removeItem("token");
-  window.location.href = "/login";
+  Navigate('/login');
 };
 const Header = () => {
   return (
