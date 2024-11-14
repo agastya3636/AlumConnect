28b95f77ea592bc7e4094b0700dac2f492849ef9
Agastya Kumar Yadav <124435030+agastya3636@users.noreply.github.com>
2024-11-14 16:18:08 +0530

    complete opensource

--- frontend/src/features/opensource/AddProject.jsx
+++ frontend/src/features/opensource/AddProject.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const AddProjectForm = () => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
