cda2f2b2c741242dc956d2fa5aaa5652872ce482
Agastya Kumar Yadav <124435030+agastya3636@users.noreply.github.com>
2024-11-18 19:52:24 +0530

    logout button fix

--- frontend/src/ui/Header.jsx
+++ frontend/src/ui/Header.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import { FiLogOut, FiUser } from "react-icons/fi";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
-function handleLogout() {
-  const Navigate = useNavigate();
-  alert("Logging out");
-  localStorage.removeItem("token");
-  sessionStorage.removeItem("token");
-  Navigate('/login');
-};
 const Header = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    alert("Logging out");
+    localStorage.removeItem("token");
+    sessionStorage.removeItem("token");
+    navigate("/login"); // Navigate to the login page
+  };
+
   return (
     <header className="bg-gray-100 py-4 px-12 flex items-center justify-end">
       <div className="flex space-x-4">
         <button className="text-gray-600 hover:text-gray-800">
           <FiUser size={24} />
         </button>
-        <button className="text-gray-600 hover:text-gray-800"
+        <button
+          className="text-gray-600 hover:text-gray-800"
           onClick={handleLogout}
         >
           <FiLogOut size={24} />
